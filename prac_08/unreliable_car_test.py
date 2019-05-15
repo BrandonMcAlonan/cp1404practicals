87f01ee76b4dffa41f52f44dd17a0b9958f78904
BrandonMcAlonan <brandon.mcalonan@my.jcu.edu.au>
2019-05-15 23:43:50 +1000

    Add unreliable_car_test.py

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,11 @@
+from unreliable_car import UnreliableCar
+
+
+def main():
+
+    unreliable_car = UnreliableCar("Unreliable 1", 50, 50)
+    unreliable_car.drive(30)
+    print(unreliable_car)
+
+
+main()
