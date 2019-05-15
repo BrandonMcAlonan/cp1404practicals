28c5798bab0774f68be243aa7c9633d799b40ef3
BrandonMcAlonan <brandon.mcalonan@my.jcu.edu.au>
2019-05-15 23:35:34 +1000

    Fix dynamic label generation

--- prac_07/simple_dynamic_app.py
+++ prac_07/simple_dynamic_app.py
@@ -1,19 +1,23 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
 
 widget_list = ["This", "Is a", "Dynamic", "App"]
 
 
 class SimpleDynamicApp(App):
+
     def build(self):
         self.title = "Simple Dynamic App"
         self.root = Builder.load_file('simple_dynamic_app.kv')
+        self.create_widgets()
         return self.root
 
-    def create_dynamic_widgets(self):
-        for name in self.widget_list:
-            temp_label = Labe1(text=name)
+    def create_widgets(self):
+        for name in widget_list:
+            temp_label = Label(text=name)
             self.root.ids.widgets_here.add_widget(temp_label)
 
 
 SimpleDynamicApp().run()
+
