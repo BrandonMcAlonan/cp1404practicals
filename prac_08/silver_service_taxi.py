feeccefc5a78386ef08aba2604b68f94756f9408
BrandonMcAlonan <brandon.mcalonan@my.jcu.edu.au>
2019-05-16 00:23:35 +1000

    Add silver_service_taxi_test.py

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -2,21 +2,16 @@ from prac_08.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
-    flag_fall = 4.50
-    number_of_fares = 0
+    flag_fall = 4.5
 
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = fanciness
-        self.current_fare_distance = 0
-        self.price_per_km = Taxi.price_per_km * self.fanciness
+        self.price_per_km *= fanciness
 
     def __str__(self):
-        """Return a string like a Car but with current fare distance."""
-        return "{}, {}km on current fare, ${:.2f}/km plus flagfall of ${:.2f}".format(super().__str__(),
-                                                             self.current_fare_distance, self.price_per_km,
-                                                             self.flag_fall * self.number_of_fares)
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(),
+                                                    self.flag_fall)
 
-    def start_fare(self):
-        self.current_fare_distance = 0
-        self.number_of_fares += 1
+    def get_fare(self):
+        return self.flag_fall + super().get_fare()
